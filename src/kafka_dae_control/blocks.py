0330cd3843dcaeff338817e009eea0c9c588e13f
Jack Harper <harps912@gmail.com>
2026-04-23 10:37:03 +0100

    encode char_value

--- src/kafka_dae_control/blocks.py
+++ src/kafka_dae_control/blocks.py
@@ -9,9 +9,10 @@ from kafka_dae_control.data import Data
 logger = logging.getLogger(__name__)
 
 
-def update_blocks(prefix: str, data: "Data", *, char_value: bytes, **___: int) -> None:
+def update_blocks(prefix: str, data: "Data", *, char_value: str, **___: int) -> None:
     """Update the list of blocks in the data class."""
-    logger.debug("blocks_hexed: %s (char), %s (str)", char_value)
-    blocks_unhexed = dehex_decompress_and_dejson(char_value)
+    encoded_val = char_value.encode("utf-8")
+    logger.debug("blocks_hexed: %s (char), %s (bytes)", char_value, encoded_val)
+    blocks_unhexed = dehex_decompress_and_dejson(encoded_val)
     logger.debug("blocks_unhexed: %s", blocks_unhexed)
     data.blocks = [f"{prefix}CS:SB:{x}" for x in blocks_unhexed]
