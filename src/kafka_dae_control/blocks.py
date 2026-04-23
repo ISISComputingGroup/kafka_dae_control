7a36ec84f0254b0cedf0d2013f1d004e29f0f285
Jack Harper <harps912@gmail.com>
2026-04-23 10:35:34 +0100

    remove value

--- src/kafka_dae_control/blocks.py
+++ src/kafka_dae_control/blocks.py
@@ -9,11 +9,9 @@ from kafka_dae_control.data import Data
 logger = logging.getLogger(__name__)
 
 
-def update_blocks(
-    prefix: str, data: "Data", *, value: bytes, char_value: str, **___: int
-) -> None:
+def update_blocks(prefix: str, data: "Data", *, char_value: bytes, **___: int) -> None:
     """Update the list of blocks in the data class."""
-    logger.debug("blocks_hexed: %s (char), %s (str)", char_value, value)
-    blocks_unhexed = dehex_decompress_and_dejson(value)
+    logger.debug("blocks_hexed: %s (char), %s (str)", char_value)
+    blocks_unhexed = dehex_decompress_and_dejson(char_value)
     logger.debug("blocks_unhexed: %s", blocks_unhexed)
     data.blocks = [f"{prefix}CS:SB:{x}" for x in blocks_unhexed]
